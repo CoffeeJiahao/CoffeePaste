e4f64f34b711b2787a75e39c51f5a331a2f4861c
符步思 <fuyao.22@bytedance.com>
2026-03-26 18:52:38 +0800

    base

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 cd $(dirname $0)
+pkill -f CoffeePaste.app
 rm -rf '/Applications/CoffeePaste.app'
 cp -r 'build/Build/Products/Release/CoffeePaste.app' '/Applications/'
+open '/Applications/CoffeePaste.app'
